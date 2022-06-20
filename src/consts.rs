fe7e62324134a75c7f14badafb99769f06683eac
Shyveon <42724515+Shyveon@users.noreply.github.com>
2022-06-20 08:21:41 -0300

    a

--- src/consts.rs
+++ src/consts.rs
@@ -11,7 +11,7 @@ pub const DICAS: [&'static str; 10] = [
     "10. Tolomeo tem medo de decisões difíceis."
 ];
 
-pub const HISTORIA: &str = "Alice, sempre gostou de viver a vida sem limites.\nEm uma bela noite, voltando de mais uma noite de extravagâncias.\n Alice descobre que os freios do carro de seu pai que ele tanto cuidava e revisava não iriam ser bons o suficiente para salvar-lá\n, pobre carrinho,\n nunca teve chance.\n Alice bateu diretamente em um árvore, apesar de seus ferimentos não parecerem graves.\n A força da batida fez com que sua cabeça balançasse como um chicote e transformou seu cérebro em papinha...\nAlice, acorda em uma sala escura iluminada por velas com chamas que não tremulam e nem queimam a parafína.\n Quando ela percebe um imenso olho aparece e lhe conta sobre a situação em que ela está.";
+pub const HISTORIA: &str = "Alice, sempre gostou de viver a vida sem limites.\nEm uma bela noite, voltando de mais uma noite de extravagâncias.\n Alice descobre que os freios do carro de seu pai que ele tanto cuidava e revisava não iriam ser bons o suficiente para salvar-lá, pobre carrinho nunca teve chance.\n Alice bateu diretamente em um árvore, apesar de seus ferimentos não parecerem graves.\n A força da batida fez com que sua cabeça balançasse como um chicote e transformou seu cérebro em papinha...\nAlice, acorda em uma sala escura iluminada por velas com chamas que não tremulam e nem queimam a parafína.\n Quando ela percebe um imenso olho aparece e lhe conta sobre a situação em que ela está.\n O olho explica como ela morreu e que ela tem apenas uma chance de voltar a vida.\n Quatros portas se apresentam a ela, apenas uma porta é a correta, será que você consegue escolher a certa?";
 
 pub const PORTA: &str = "__________
 |  __  __  |
